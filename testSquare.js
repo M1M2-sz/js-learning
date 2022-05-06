d6a7877fd1ae41ee2b0ff459e1935df85a24088c
M1M2-sz <michael1992avia@gmail.com>
2022-05-06 14:24:38 +0300

    Initial commit

---
+++ testSquare.js
@@ -0,0 +1,17 @@
+var 
+x0 = 0 , 
+y0 = 0 , 
+R = 10 ,
+x1 = 10 ,
+y1 = 10 ;
+
+
+if ( x1 >= R + x0 , y1 >= R + y0 ) {
+    console.log ( 'The value is not included in the circle' )
+} else if ( x1 <= R + x0 , y1 >= R + y0 ) {
+    console.log ( 'The value enters in the circle' )   
+} else ( 'Enters in the circle!' )
+
+
+
+
