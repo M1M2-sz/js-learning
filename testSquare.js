cb10bc14020567433098b5107178142b5a3332f8
M1M2-sz <michael1992avia@gmail.com>
2022-05-06 16:07:13 +0300

    ++ new variants

--- testSquare.js
+++ testSquare.js
@@ -14,7 +14,26 @@ else
 {console.log ( 'The value enters in the circle!' );}
 
 
-// variant 2 - construction '?' - true/false //
+// variant 2 - construction else if //
+
+var 
+x0 = 0 , 
+y0 = 0 , 
+R = 10 ,
+x1 = 10 ,
+y1 = 10 ;
+
+if ( x1 >= R + x0 , y1 >= R + y0 ) 
+{console.log ( 'The value is not included in the circle' );} 
+
+else if ( x1 <= R + x0 , y1 <= R + y0 ) 
+{console.log ( 'The value enters in the circle!' );} 
+
+else 
+{console.log ( 'WOW!' );}
+
+
+// variant 3 - construction '?' - true/false //
 
 var 
 x0 = 0 , 
@@ -29,3 +48,18 @@ let result = ( x1 >= R + x0 , y1 >= R + y0 ) ? false : true ;
 result;
 
 
+// variant 4 -  double construction '?' //
+
+var 
+x0 = 0 , 
+y0 = 0 , 
+R = 10 ,
+x1 = 10 ,
+y1 = 10 ;
+
+let result2 = 
+( x1 >= R + x0 , y1 >= R + y0 ) ? 'The value is not included in the circle' :
+( x1 <= R + x0 , y1 <= R + y0 ) ? 'The value enters in the circle!' :
+'WOW!';
+
+result2;
