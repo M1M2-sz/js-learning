e5245753ee805002beadeaa6f01016ecc3842b73
M1M2-sz <michael1992avia@gmail.com>
2022-05-06 15:28:14 +0300

    Update testSquare.js

--- testSquare.js
+++ testSquare.js
@@ -8,7 +8,7 @@ y1 = 10 ;
 
 if ( x1 >= R + x0 , y1 >= R + y0 ) 
 {console.log ( 'The value is not included in the circle' )} 
-else if ( x1 <= R + x0 , y1 >= R + y0 ) 
+else if ( x1 <= R + x0 , y1 <= R + y0 ) 
 {console.log ( 'The value enters in the circle' )} 
 else ( 'Enters in the circle!' )
 
