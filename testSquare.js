9c0bb56f8db8ec6ee2cdb16842b5c23591bd3146
M1M2-sz <michael1992avia@gmail.com>
2022-05-06 16:35:56 +0300

    square problem 1

--- testSquare.js
+++ testSquare.js
@@ -63,3 +63,19 @@ let result2 =
 'WOW!';
 
 result2;
+
+
+// square problem //
+
+var 
+x0 = 0 , 
+y0 = 0 , 
+R = 10 ,
+x1 = 10 ,
+y1 = 10 ;
+
+if ( Math.sqrt ( x1 * x1 + y1 * y1 ) >= R ) 
+{console.log ( 'The value is not included in the circle' );} 
+
+else 
+{console.log ( 'The value enters in the circle!' );}
